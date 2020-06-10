296373fc9fe7ad8910eb278bb8f08bbd240d358b
Zestlifia <zestlifia123@gmail.com>
2020-06-11 01:14:41 +0800

    1.Simple Bean 2.Path Variable

--- src/main/java/com/zestlifia/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/zestlifia/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.zestlifia.rest.webservices.restfulwebservices;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +10,13 @@ public class HelloWorldController {
     @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     public String helloWorld() {
         return "Hello world - Zestlifia";
+    }
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello world bean - Zestlifia");
+    }
+    @RequestMapping(method = RequestMethod.GET, path= "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("hello path variable, %s", name));
     }
 }
